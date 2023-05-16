4d39c6a636b5b42c9bb07be7c9937867f4fe11df
Marine Veniat <marine.veniat@hotmail.fr>
2023-05-16 18:08:44 +0200

    add filters + working filters

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
@@ -1,48 +1,103 @@
-//Récupérer les photos
+//Recover photos
 const galleryApi = document.querySelector(".gallery");
+
+function displayProject(title, imageUrl, filter)
+{
+    // Create HTML element
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+    const figureCaption = document.createElement("figcaption");
+
+    //Create class for figures 
+    filter = filter.replace(/\s+/g, '').replace("&", "");
+    figure.classList.add(filter);
+
+    // Create hierarchy
+    figure.appendChild(image);
+    figure.appendChild(figureCaption);
+    galleryApi.appendChild(figure);
+
+    // give an image and a title to the created elements
+    image.setAttribute("src", imageUrl);
+    figureCaption.innerText = title;
+}
+
+function addFilters(filtersName)
+{
+    //Creation and position of the div containing buttons
+    const filter = document.getElementById("portfolio");
+    const positionInId = filter.getElementsByTagName("h2")[0];
+    const buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("buttonfilter");
+    positionInId.insertAdjacentElement('afterend', buttonDiv);
+    
+    for (let i =0; i < filtersName.length; i++) 
+    {
+        let buttonText = filtersName[i];
+
+        //Creation and position of boutons 
+        const buttonFilter = document.createElement("button");
+        buttonDiv.appendChild(buttonFilter);
+
+        //Adding texte inside the bouton 
+        buttonFilter.innerText = buttonText;
+
+        //Adding filter on the main button
+        if(i === 0)
+        {
+            buttonFilter.classList.add('filterselected');
+        }
+
+        //Adding click and sort
+        const projectsToHide = document.querySelectorAll('figure');
+
+        buttonText = buttonText.replace(/\s+/g, '').replace("&", "");
+
+        let projectsToShow;
+        if (buttonText === 'Tous') {
+            projectsToShow = document.querySelectorAll('figure');
+        }
+        else {
+            projectsToShow = document.querySelectorAll("." + buttonText);
+        }
+
+
+        buttonFilter.addEventListener('click', function () {
+            const allButtons = document.querySelectorAll('.buttonfilter button');
+            for (let l = 0; l < allButtons.length; l++) 
+            {
+                allButtons[l].classList.remove('filterselected');
+            }
+
+            for(let j = 0; j < projectsToHide.length; j++)
+            {
+                projectsToHide[j].style.display = "none";
+            }
+
+            for(let k = 0; k < projectsToShow.length; k++)
+            {
+                projectsToShow[k].style.display = "block";
+            }
+            buttonFilter.classList.add('filterselected');
+        });
+    }
+}
 
 fetch("http://localhost:5678/api/works")
 .then (function(response) { 
     return response.json();
 })
 .then(function(json) {
-    console.log(json);
-
-    for (let project of json) {
-        
-        const figure = document.createElement("figure");
-        const image = document.createElement("img");
-        const figureCaption = document.createElement("figcaption");
-
-        image.setAttribute("src", project.imageUrl);
-        figureCaption.innerText = project.title;
-
-        figure.appendChild(image);
-        figure.appendChild(figureCaption);
-        galleryApi.appendChild(figure);
+    for (let object of json) 
+    {
+        displayProject(object.title, object.imageUrl, object.category.name);
     }
 
-    let namesFilter = json.map(project => project.category.name); //Retourne la valeur des noms des filtres sous forme de liste
-    console.log(namesFilter);
+    let namesFilter = json.map(object => object.category.name); //Return the value of filters's name as a list
 
-    let uniqueFilter = Array.from(new Set(namesFilter)); //Trier et stocker des valeurs uniques + retourner un tableau
+    let uniqueFilter = Array.from(new Set(namesFilter)); //Sort and store unique values ​​+ return an array
     uniqueFilter.unshift("Tous");
 
-    const filter = document.getElementById("portfolio");
-    const positionInId = filter.getElementsByTagName("h2")[0];
-    const buttonDiv = document.createElement("div");
-    buttonDiv.classList.add("buttonfilter");
-    positionInId.appendChild(buttonDiv);
+    addFilters(uniqueFilter);
 
-    for (let i =0; i < uniqueFilter.length; i++) 
-    {
-        let buttonText = uniqueFilter[i];
-
-        //Création et position du bouton 
-        const buttonFilter = document.createElement("button");
-        buttonDiv.appendChild(buttonFilter);
-
-        //Ajout du texte sur le bouton 
-        buttonFilter.innerText = buttonText;
-    }
 });
